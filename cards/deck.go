b1def07dba4227cc70cf02b25dc081f3acb6e7ca
Dave <davidkhauss@gmail.com>
2024-04-25 20:05:44 +0000

    file i/o implemented

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // create 'deck' type, slice of strings
 
@@ -28,4 +32,24 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	deckStr := strings.Split(string(bs), ",")
+
+	return deck(deckStr)
 }
--- cards/main.go
+++ cards/main.go
@@ -5,9 +5,10 @@ import "fmt"
 var deckSize int = 52
 
 func main() {
-	/*	var card string = "Ace of Spades"
-		card2 := "Ace of Diamonds"
-		card2 = "Five of Diamonds"
+	var card string = "Ace of Spades"
+	card2 := "Ace of Diamonds"
+	card2 = "Five of Diamonds"
+	/*
 		fmt.Println(card, card2)
 
 		deckSize = 52
@@ -30,16 +31,21 @@ func main() {
 			fmt.Println(i, ":", cards[i])
 		}
 	*/
-	//myDeck := deck{card, card2, card3}
-	//myDeck.print()
+	myDeck := deck{card, card2}
+	myDeck.print()
 
 	cards := newDeck()
+	fmt.Println(cards.toString())
 
 	hand, cards := deal(cards, 5)
-	fmt.Println("Cards:")
-	hand.print()
-	fmt.Println("Deck:")
-	cards.print()
+	fmt.Println("Cards:", hand.toString())
+	fmt.Println("Deck:", cards.toString())
+
+	cards.saveToFile("myCards")
+
+	fmt.Println("\ncardsFromFile")
+	cardsFromFile := newDeckFromFile("myCards")
+	fmt.Println(cardsFromFile.toString())
 }
 
 func newCard() string {
