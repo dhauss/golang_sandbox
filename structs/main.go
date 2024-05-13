7bac2c327739304d5c6435d037f413417f1a21bd
Dave <davidkhauss@gmail.com>
2024-05-13 15:07:23 +0000

    contact struct added

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,35 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip   int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
+	contact := contactInfo{email: "alex@email.com", zip: 12345}
+	alex := person{"Alex", "Anderson", contact}
+	fmt.Println(alex)
+
+	alex2 := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact:   contact,
+	}
+	fmt.Println(alex2.firstName)
+
+	var alex3 person
+	fmt.Printf("%+v\n", alex3)
+	alex3.firstName = "Alex"
+	alex3.lastName = "Anderson"
+	alex3.contact = contact
+	fmt.Printf("%+v\n", alex3)
 
 }
