b0967700a01b00cd083a708c365bd267882dfb52
Dave <davidkhauss@gmail.com>
2024-05-22 14:00:06 +0000

    triangle test added

--- shapes/shape_test.go
+++ shapes/shape_test.go
@@ -6,7 +6,14 @@ func TestNewSquare(t *testing.T) {
 	s := square{sideLength: 2}
 
 	if s.getArea() != 4 {
-		t.Errorf("Expected area of 4, got %v\n", s.getArea())
+		t.Errorf("Expected square's area to be 4, got %v\n", s.getArea())
 	}
+}
 
+func TestNewTriangle(t *testing.T) {
+	tri := triangle{base: 4, height: 4}
+
+	if tri.getArea() != 8 {
+		t.Errorf("Expected triangle's area to be 8, got %v\n", tri.getArea())
+	}
 }
